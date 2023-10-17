5a0734a43420ba4ffeae3adcae40e8b9d7e6b3a8
agustinrosa22 <agustinrosa1234@gmail.com>
2023-10-17 11:06:08 -0300

    ReposList

---
+++ src/Components/RepositoriesList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import repositories from "./data/repositories";
+
+const ReposList = ()=> {
+    return (
+        <View>
+        {repositories.map(repo => (
+            <View key={repo.id}>
+                <Text>{repo.id}</Text>
+                </View>
+        ))}
+        </View>
+    )
+}
+export default ReposList
